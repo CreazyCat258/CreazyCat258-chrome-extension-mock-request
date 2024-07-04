5f526acb86e568eb8f5008c9924fac5c10a5f5c4
zhangyanze <zhangyanze@meituan.com>
2024-07-04 21:56:53 +0800

    feat: 删除无用代码

--- service_worker.js
+++ service_worker.js
@@ -1,20 +1,6 @@
 // panel窗口的唯一标识
 let panelWindowId = null;
 
-// 【事件监听】插件安装/更新
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("插件-安装")
-  // 页面邮件菜单，添加菜单项
-  chrome.contextMenus.create({
-    "id": "sampleContextMenu",
-    "title": "RequestMock插件",
-    "contexts": ["page"],
-  });
-
-  // 设置弹窗页面
-  // chrome.action.setPopup({ popup: 'popup/index.html' });
-});
-
 // 【事件监听】浏览区工具栏点击插件
 // 只有在没有设置 popup 弹窗页面的时候才会被触发
 chrome.action.onClicked.addListener((tab) => {
@@ -32,15 +18,13 @@ chrome.action.onClicked.addListener((tab) => {
   } else {
     createPanelWindow();
   }
-
 });
-
 
 // 创建面板窗口
 function createPanelWindow() {
   try {
     chrome.windows.create({
-      url: "./panel/dist/index.html",
+      url: "./test.html",
       type: "panel",
     }, (win) => {
       panelWindowId = win.id;
